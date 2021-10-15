80f8a1500a9ef99166cc3c5307498b927f44264a
Kiky Chow <kikychow@192.168.1.129>
2021-10-15 17:59:31 +0100

    Implemented flip cards for project section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,13 +4,23 @@ function Project(props) {
   const image = process.env.PUBLIC_URL + "/images/pintos3.jpg";
   return (
     <div className="col-lg-3 col-md-6 col-sm-12 col-12">
-      <div
-        className="project-card"
-        style={{
-          backgroundImage: `radial-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)), url(${props.image})`,
-        }}
-      >
-        <p className="project-name">{props.title}</p>
+      <div className="project-card flip-card">
+        <div className="flip-card-inner">
+          <div
+            className="flip-card-front"
+            style={{
+              backgroundImage: `radial-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)), url(${props.image})`,
+            }}
+          >
+            <p className="project-name">{props.title}</p>
+          </div>
+          <div className="flip-card-back">
+            <div className="center-block">
+              {/* <h4>{props.title}</h4> */}
+              <p>{props.description}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/ProjectsData.jsx
+++ src/components/ProjectsData.jsx
@@ -4,21 +4,21 @@ const ProjectsData = [
     image: process.env.PUBLIC_URL + "/images/lostnfound.jpg",
     title: "Lost & Found Web App",
     url: "",
-    description: "Write something here",
+    description: "A platform where people can report lost items and notify others if any of them are found.",
   },
   {
     id: 4,
     image: process.env.PUBLIC_URL + "/images/WACC.jpeg",
     title: "WACC Compiler",
     url: "",
-    description: "Write something here",
+    description: "Built a lexer, a parser and a semantic analyser for WACC programs and built a code generator for the ARM11 architecture",
   },
   {
     id: 3,
     image: process.env.PUBLIC_URL + "/images/pintos.jpg",
     title: "Pintos Operating System",
     url: "",
-    description: "Testing hahaha",
+    description: "A simple operating system framework for the 80x86 architecture. Strengthened supports in kernel threads, running user programs and file system and implemented the use of virtual memory",
   },
   {
     id: 2,
@@ -26,13 +26,13 @@ const ProjectsData = [
     title: "Propositional Logic Evaluator",
     url: "",
     description:
-      "A logic compiler built using a parse tree that takes logic equations and assignments as input, and returns an assembly file for our ARM23 Project",
+      "A logic compiler built using a parse tree that takes logic equations and assignments as input, and returns an assembly file..",
   },
   {
     id: 1,
     image: process.env.PUBLIC_URL + "/images/GOL.png",
     title: "Conway's Game of Life",
-    description: "",
+    description: "Simulates cell interactions in ‘Conway's Game of Life’ on a 2-dimensional grid of squared cells",
   },
 ];
 
